d45f542775f707e7ca4109e6403ca5b75657ea16
memematvaz <memematvaz@gmail.com>
2020-08-14 11:50:45 +0200

    better background black

--- js/main.js
+++ js/main.js
@@ -4,29 +4,38 @@ const iconClosePhone = document.querySelector('#pop-up-close-phone')
 const iconCloseWhere = document.querySelector('#pop-up-close-where')
 const popupPhone = document.querySelector('#pop-up-phone')
 const popupWhere = document.querySelector('#pop-up-where')
-
+const black = document.querySelector('#pop-up-black')
 const more = document.querySelector('#more')
 const card = document.querySelector('#card')
 
 function openPopupPhone(event) {
     popupPhone.classList.remove('none');
     popupPhone.classList.add('pop-up__container');
+    black.classList.remove('none');
+    black.classList.add('pop-up__black');
 }
 
 function closePopupPhone(event) {
     popupPhone.classList.add('none');
     popupPhone.classList.remove('pop-up__container');
+    black.classList.add('none');
+    black.classList.remove('pop-up__black');
 }
 
 function openPopupWhere(event) {
     popupWhere.classList.remove('none');
     popupWhere.classList.add('pop-up__container');
+    black.classList.remove('none');
+    black.classList.add('pop-up__black');
 }
 
 function closePopupWhere(event) {
     popupWhere.classList.add('none');
     popupWhere.classList.remove('pop-up__container');
+    black.classList.add('none');
+    black.classList.remove('pop-up__black');
 }
+
 
 function toggleCard(event) {
     if(card.classList.contains('none')=== true){
